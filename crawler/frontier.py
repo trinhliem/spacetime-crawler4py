f7d8dfd07750f35dcdebfa90292f712560f7037b
Quang Dinh Tue Tran <quangdt1@uci.edu>
2026-02-06 19:56:05 -0800

    Edit the frontier.py, worker.py, download.py, config.ini, to implement the EC2 - politeness - current thread is 2

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -1,18 +1,23 @@
 import os
 import shelve
 
-from threading import Thread, RLock
+from threading import Thread, Lock
 from queue import Queue, Empty
 
 from utils import get_logger, get_urlhash, normalize
 from scraper import is_valid
 
+#Reference for politeness implementation: https://dev.to/h0tb0x/webcrawling-is-just-a-brute-force-algorithm-2meg, https://www.packtpub.com/en-us/product/python-web-scraping-9781782164364/chapter/1-introduction-to-web-scraping-1/section/crawling-your-first-website-ch01lvl1sec05, https://realpython.com/python-thread-lock/    
 class Frontier(object):
     def __init__(self, config, restart):
         self.logger = get_logger("FRONTIER")
         self.config = config
-        self.to_be_downloaded = list() # initialize the todo list of URLs
-        
+
+        # Thread-safe queue for URLs to be downloaded, and set for deduplication.
+        self.to_be_downloaded = Queue() # initialize the frontier as a queue; workers will pop from this queue to get the next URL to crawl
+        self.lock = Lock() # lock for synchronizing access to the frontier
+
+        #Persistent storage for discovered URLs and their completion status. Keyed by URL hash.
         if not os.path.exists(self.config.save_file) and not restart:
             # Save file does not exist, but request to load save.
             self.logger.info(
@@ -23,8 +28,11 @@ class Frontier(object):
             self.logger.info(
                 f"Found save file {self.config.save_file}, deleting it.")
             os.remove(self.config.save_file)
+
         # Load existing save file, or create one if it does not exist.
         self.save = shelve.open(self.config.save_file)
+
+        # If restart, seed the frontier with the seed urls. Otherwise, populate the frontier with the urls in the save file that are not marked as completed.
         if restart:
             for url in self.config.seed_urls:
                 self.add_url(url) # if restarting, add each seed to the frontier
@@ -39,34 +47,41 @@ class Frontier(object):
         ''' This function can be overridden for alternate saving techniques. '''
         total_count = len(self.save) # how many URLs hav been discovered 
         tbd_count = 0 # how many are still pending 
+
         for url, completed in self.save.values():
             if not completed and is_valid(url):
                 self.to_be_downloaded.append(url) # add to todolist
                 tbd_count += 1
+
         self.logger.info(
             f"Found {tbd_count} urls to be downloaded from {total_count} "
             f"total urls discovered.")
 
     def get_tbd_url(self):
         try:
-            return self.to_be_downloaded.pop() # frontier uses stack; if empty, return None; Workers stop when None is returned
-        except IndexError:
+            return self.to_be_downloaded.get(timeout=3) # block for 3 seconds waiting for a URL to be available; if none is available, raise Empty exceptiona and return None to signal that the frontier is empty or workers are taking too long to add new URLs.
+        except Empty:
             return None
 
     def add_url(self, url):
         url = normalize(url) # normalize so same page doesn't appear in multiple forms
-        urlhash = get_urlhash(url) # compute hash key for storage
-        if urlhash not in self.save:
-            self.save[urlhash] = (url, False) # store hash key as not completed yet
-            self.save.sync() # flush to disk immediately
-            self.to_be_downloaded.append(url) # add to todo list for Workers
+        urlhash = get_urlhash(url) # compute hash key for 
+
+        with self.lock: # synchronize access to the frontier        
+            if urlhash not in self.save:
+                self.save[urlhash] = (url, False) # store hash key as not completed yet
+                self.save.sync() # flush to disk immediately
+                self.to_be_downloaded.put(url) # add to frontier for workers to crawl
     
     def mark_url_complete(self, url):
         urlhash = get_urlhash(url)
-        if urlhash not in self.save:
-            # This should not happen.
-            self.logger.error(
-                f"Completed url {url}, but have not seen it before.")
 
-        self.save[urlhash] = (url, True) # update status to completed
-        self.save.sync() # flush to disk
+        with self.lock: # synchronize access to the frontier
+            if urlhash not in self.save:
+                # This should not happen.
+                self.logger.error(
+                    f"Completed url {url}, but have not seen it before.")
+                return
+
+            self.save[urlhash] = (url, True) # update status to completed
+            self.save.sync() # flush to disk
--- crawler/worker.py
+++ crawler/worker.py
@@ -6,15 +6,18 @@ from utils import get_logger
 import scraper
 import time
 
-
+#Reference for politeness implementation: https://dev.to/h0tb0x/webcrawling-is-just-a-brute-force-algorithm-2meg, https://www.packtpub.com/en-us/product/python-web-scraping-9781782164364/chapter/1-introduction-to-web-scraping-1/section/crawling-your-first-website-ch01lvl1sec05, https://realpython.com/python-thread-lock/    
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
+        
+        # We set daemon to True so that the worker threads will automatically exit when the main thread exits. This is important for clean shutdown of the crawler.
         super().__init__(daemon=True)
         
     def run(self):
@@ -23,12 +26,19 @@ class Worker(Thread):
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+
             resp = download(tbd_url, self.config, self.logger)
+            if not resp:
+                self.logger.info(f"Failed to download {tbd_url} using cache {self.config.cache_server}.")
+                self.frontier.mark_url_complete(tbd_url) # mark as complete to avoid retrying indefinitely
+                continue
+
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
             scraped_urls = scraper.scraper(tbd_url, resp) # return list of URLs to add back to frontier
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
+
             self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            #time.sleep(self.config.time_delay)
