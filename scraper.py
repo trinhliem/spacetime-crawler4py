b4edf5e0ba5c717ea9b6fda166786ea729f3af47
Trinh Nguyen Do <trinhliemnguyendo@gmail.com>
2026-02-07 16:18:06 -0800

    reconfigured logging to show all info logs in stdout

--- scraper.py
+++ scraper.py
@@ -44,15 +44,17 @@ STOPWORDS = {
 }
 SUBDOMAIN_COUNTS: dict[str, int] = {}
 
-# configure logging
+# configure logging, documentation: https://docs.python.org/3/howto/logging.html
 logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    "%(asctime)s | %(levelname)s | %(message)s"
-)
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+if not logger.handlers:
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
 
 def scraper(url, resp):
